c3d9a8047c5c04882e88c1e488da38e4a9f9e6c8
Miles Wentworth <miles@Miless-MacBook.local>
2017-06-13 22:58:28 -0700

    Added new example file, strings1.rb. Contains one example of printing variables.

---
+++ expressions/strings1.rb
@@ -0,0 +1,8 @@
+
+# Here is a different way to print a variable.
+# Note that this only works for double quotes.
+a = 41
+print "Printing variable 'a' ...\n\n"
+print "With double quotes: #{a}\n"
+print 'With single quotes: #{a}\n'
+print "\n"
