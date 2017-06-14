02d5e912903df05d0bc627a45e9572eb7f1b68db
Miles Wentworth <miles@Miless-MacBook.local>
2017-06-13 22:43:26 -0700

    Added first files. A hello world script and a simple script using variables.

---
+++ expressions/expressions1.rb
@@ -0,0 +1,15 @@
+
+# Example variables created.
+a = 5
+b = 10
+c = 11
+
+printf("a = %d\nb = %d\nc = %d\n", a, b, c);
+
+# Yay!! Dynamic typing :)
+b = "I'm a string now"
+c = nil
+
+print "We can print strings and nil objects!\n"
+print 'b = "' + b + '"' + "\n"
+print 'c = "' + c.to_s + '"' + "\n"
