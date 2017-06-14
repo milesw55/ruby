1eb5979df7eafc3b3bf180b065a9f2a346f76b12
Miles Wentworth <miles@Miless-MacBook.local>
2017-06-13 23:15:46 -0700

    Added header comment to strings1.rb. Also added shebang and preceded existing example with the simplest example.

--- expressions/strings1.rb
+++ expressions/strings1.rb
@@ -1,3 +1,17 @@
+#!/usr/bin/ruby
+##
+#  @file strings1.rb
+#  
+#  This file contains examples of how variables can be
+#  printed within strings.
+#
+#  @author Miles Wentworth
+#
+
+# Here is a simple way to concatenate a variable to string.
+a = 41
+print "The value of 'a' is: " + a.to_s
+print "\n\n"
 
 # Here is a different way to print a variable.
 # Note that this only works for double quotes.
