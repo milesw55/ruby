03c14699b401eb5b0f6edcb8f43bd12443cc32a1
Miles Wentworth <miles@Miless-MacBook.local>
2017-06-14 18:19:41 -0700

    Added better comments. Added another printing example to strings1.rb. Cleaned up the format of the other examples so they are all consistent.

--- expressions/strings1.rb
+++ expressions/strings1.rb
@@ -10,13 +10,33 @@
 
 # Here is a simple way to concatenate a variable to string.
 a = 41
-print "The value of 'a' is: " + a.to_s
+print "Printing variable 'a' using concatenation ...\n\n"
+print "The value of 'a' is: " + a.to_s + "\n"
 print "\n\n"
 
 # Here is a different way to print a variable.
 # Note that this only works for double quotes.
-a = 41
-print "Printing variable 'a' ...\n\n"
-print "With double quotes: #{a}\n"
-print 'With single quotes: #{a}\n'
-print "\n"
+b = 99
+print "Printing variable 'b' using substitution ...\n\n"
+print "With double quotes: #{b}\n"
+print 'With single quotes: #{b}\n' + "\n"
+print "\n\n"
+
+# Here we are printing using << notation.
+c = -101
+s = <<ENDER
+Printing variable 'c' using << notation ...
+
+The value of 'c' is: #{c}
+
+ENDER
+
+print s
+
+#Here we are printing using multi-line string.
+d = 23
+print "
+Printing the value of 'd' using a multi-line string ...
+
+The value of 'd' is: " + d.to_s + "\n"
+print "\n\n"
